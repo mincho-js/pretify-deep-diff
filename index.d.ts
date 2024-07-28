92512a06880b89c7e819664013042184bd12458b
JeongJun <rgfdds98@gmail.com>
2024-07-28 16:15:22 +0900

    Chore: publish package
    - https://github.com/mincho-js/mincho/issues/38
    - Fix: rename declare module name

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,28 @@
-declare module 'pretify-deep-diff' {
-    interface Options {
-        locale?: 'en' | 'ru';
-        customTexts?: {
-            added?: string;
-            deleted?: string;
-            edited?: string;
-            arrayChange?: string;
-            addedTitle?: string;
-            deletedTitle?: string;
-            editedTitle?: string;
-            arrayChangeTitle?: string;
-        };
-    }
+declare module "@mincho-js/pretify-deep-diff" {
+  interface Options {
+    locale?: "en" | "ru";
+    customTexts?: {
+      added?: string;
+      deleted?: string;
+      edited?: string;
+      arrayChange?: string;
+      addedTitle?: string;
+      deletedTitle?: string;
+      editedTitle?: string;
+      arrayChangeTitle?: string;
+    };
+  }
 
-    interface Change {
-        kind: 'N' | 'D' | 'E' | 'A';
-        path?: (string | number)[];
-        lhs?: any;
-        rhs?: any;
-        index?: number;
-        item?: Change;
-    }
+  interface Change {
+    kind: "N" | "D" | "E" | "A";
+    path?: (string | number)[];
+    lhs?: any;
+    rhs?: any;
+    index?: number;
+    item?: Change;
+  }
 
-    function pretifyDeepDiff(changes: Change[], options?: Options): string;
+  function pretifyDeepDiff(changes: Change[], options?: Options): string;
 
-    export = pretifyDeepDiff;
+  export = pretifyDeepDiff;
 }
